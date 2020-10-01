fd424eef1fd5f029d4f1ac031cbab57ba7b227d5
Marc Steele <marcsteele2014@gmail.com>
2020-10-01 20:05:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\d+\W/) != nil
+  if phone.match(/\d{10}/) || phone.match(/\d{3}\W\d{7}/) || phone.match(/\d{3}\W\d{3}\Wd{4}/)!= nil
     return true
   else
     return
