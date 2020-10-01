be4a5a75c573e7884a4a30e5e2eea4074a5c5174
Marc Steele <marcsteele2014@gmail.com>
2020-10-01 19:02:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.grep(/\bun+\w+ing\b/) { |match|  }
+  array.push(text.grep(/\bun+\w+ing\b/)) #{ |match|  }
+  return array
 end
 
 def words_five_letters_long(text)
