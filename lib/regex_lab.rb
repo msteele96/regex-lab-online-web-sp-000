57322eab4b3dd553b6019f95a5f597ff69dec7bb
Marc Steele <marcsteele2014@gmail.com>
2020-10-01 19:00:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.grep(/\bun+\w+ing\b/)
+  text.grep(/\bun+\w+ing\b/i)
 end
 
 def words_five_letters_long(text)
