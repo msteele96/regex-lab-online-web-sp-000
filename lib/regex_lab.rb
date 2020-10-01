9425116f2951f8d8b4f1185fa0573069407a5534
Marc Steele <marcsteele2014@gmail.com>
2020-10-01 19:49:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,10 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\d{10}/) != nil
+    return true
+  else
+    return
+    false
+  end
 end
