0ab043677bbe34409bae238b59bbb47082a35617
Marc Steele <marcsteele2014@gmail.com>
2020-10-01 20:12:41 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,32 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiou]+\w+/i) != nil
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun+\w+ing\b/i)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[A-Z]/) && text.match(/\W\z/) != nil
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/) != nil
+    return true
+  else
+    return
+    false
+  end
 end
